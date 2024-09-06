2a45590b0ad3fc8a815a9c230186d275601df8e2
Quoc Thong <thongnguyennqt@gmail.com>
2024-09-06 08:08:45 +0700

    [update] special offer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,12 +1,18 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 const SpecialOffer = () => {
   return (
-    <section className="flex justify-wrap items-center max-xl:flex-col-reverse">
-       <div className="flex flex-1 flex-col">
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      
+        <div className="flex-1">
+          <img src={offer} width={773} height={678} className="object-contain w-full" alt="" />
+        </div>
+
+        <div className="flex flex-1 flex-col">
           <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg" >
-            We provide you
-            <span className="text-coral-red "> Super</span>
-            <span className="text-coral-red "> Quality </span>
-            Shoes
+            <span className="text-coral-red "> Special </span>
+            Offer Shoes
           </h2>
           <p className='mt-4 lg:max-w-lg info-text'>
           Ensuring premium comfort and style, our meticulously crafted footwear
@@ -16,10 +22,12 @@ const SpecialOffer = () => {
           <p className='mt-6 lg:max-w-lg info-text'>
           Our dedication to detail and excellence ensures your satisfaction
           </p>
-          <div className="mt-11"> 
-            <Button label="View Details" iconUrl={arrowRight} />
+          <div className="mt-11 flex flex-wrap gap-4"> 
+            <Button label="Shop now" iconUrl={arrowRight} />
+            <Button label="Learn more" backgroundColor='bg-white' boderColor='border-slate-gray' textColor='text-slate-gray'/>
           </div>
-      </div>
+        </div>
+
     </section>
   )
 }
